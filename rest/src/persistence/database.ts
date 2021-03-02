96ec22e3ccfc0c24c24e5915cd563a433be365df
Fernando Maldonado <fmaldonado824@gmail.com>
2021-03-02 12:37:31 -0600

    Added .env file

--- rest/src/persistence/database.ts
+++ rest/src/persistence/database.ts
@@ -3,11 +3,10 @@ import { connect } from "mongoose";
 class MongodbConnection {
 
     public async connectDatabase() {
-
-        if (process.env.DB_CONNECTION)
+        if (!process.env.DB_CONNECTION)
             throw new Error("No database url specified");
 
-        await connect(process.env.DB_CONNECTION!!, {
+        await connect(process.env.DB_CONNECTION!! as string, {
             useNewUrlParser: true,
             useFindAndModify: false,
             useUnifiedTopology: true
